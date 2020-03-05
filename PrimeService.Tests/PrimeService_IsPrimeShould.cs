eca9af6e9a2eae4042177421340c6fef02495737
kendrick2130 <59980052+kendrick2130@users.noreply.github.com>
2020-03-05 12:00:09 -0500

    Add project files.

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PrimeService;
+using Xunit;
+
+namespace PrimeService.Tests
+{
+   public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Theory]
+        [InlineData(11)]
+        [InlineData(13)]
+        [InlineData(17)]
+        public void IsPrime_PrimesGreaterThan10_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.True(result, $"{value} should not be prime");
+        }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(7)]
+        public void IsPrime_PrimesLessThan10_ReturnTrue(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.True(result, $"{value} should be prime");
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(6)]
+        [InlineData(8)]
+        [InlineData(9)]
+        public void IsPrime_NonPrimesLessThan10_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+
+    }
+}
---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using PrimeService;
+using System.Text;
+
+
+namespace PrimeService
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+           for(int i = 2; i <= candidate/2; i++)
+           {
+                if(candidate % i == 0)
+                {
+                    return false;
+                }
+                if(candidate <= 0)
+                {
+                    return false;
+                }
+           }
+           return true;
+
+        }
+    }
+}
